128195187ee9427d5d391277526c41d7cfa85d0b
blvckbeatle <asaproxky24@gmail.com>
2023-03-08 01:20:02 +0100

    First commit

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+const About = () => {
+  return (
+    <div>
+      <h4>Version 90210 </h4>
+      <Link to="/">Go back</Link>
+    </div>
+  );
+};
+export default About;
